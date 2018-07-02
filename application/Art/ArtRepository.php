19bcf856f0fc483cfd01e4f192062b0007aa9ee1
Frederick Vanbrabant <frederick.vanbrabant@gmail.com>
2018-07-02 10:51:04 +0200

    Add the repository

--- application/Art/ArtRepository.php
+++ application/Art/ArtRepository.php
@@ -3,6 +3,7 @@
 namespace Application\Art;
 
 use Domain\Art;
+use Domain\Art\ArtId;
 use Domain\Art\ArtRepository as ArtRepositoryInterface;
 
 class ArtRepository implements ArtRepositoryInterface
@@ -14,5 +15,20 @@ class ArtRepository implements ArtRepositoryInterface
            'description' => $description,
            'image' => $imagePath,
         ]);
+    }
+
+    public function update(
+        String $name,
+        String $description,
+        String $imagePath = null,
+        ArtId $artId
+    ): void
+    {
+        Art::where('id', $artId)
+            ->update([
+                'name' => $name,
+                'description' => $description,
+                'image' => $imagePath,
+            ]);
     }
 }
